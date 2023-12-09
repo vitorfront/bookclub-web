7094e9687cd098e1dea2712fa9fa6c72a8b7ece0
vitorfront <vitorrventurini2004@hotmail.com>
2023-12-09 14:55:01 -0300

    feat: implement first chakra elements at login screen

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react'
+import { colors } from './colors'
+
+export const theme = extendTheme({
+  colors: {
+    brand: colors
+  },
+  styles: {
+    global: {
+      'html, body': {
+        fontFamily: 'Inter'
+      }
+    }
+  }
+})
